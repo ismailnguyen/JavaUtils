7c0dd2c48c518839578410ca4224057cac35ad46
Ismail NGUYEN <me@nguyenismail.com>
2015-10-06 12:18:47 +0200

    add WebTools class

---
+++ WebTools/src/algo/text/html/WebTools.java
@@ -0,0 +1,19 @@
+package algo.text.html;
+
+import algo.text.html.interfaces.WebToolsAdapter;
+
+public class WebTools extends WebToolsAdapter {
+
+	@Override
+	public int htmlCount(String arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String htmlSubstring(String arg0, int arg1, String arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
