26ed2cb3574d6481c79c1881fbbc7758dfe49a2e
Ismail NGUYEN <me@nguyenismail.com>
2015-10-06 10:46:46 +0200

    fix sortchain compare

--- SortChain/src/org/essilab/exercices/bases/sort/SortChain.java
+++ SortChain/src/org/essilab/exercices/bases/sort/SortChain.java
@@ -1,27 +1,34 @@
 package org.essilab.exercices.bases.sort;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 
 import org.essilab.exercices.bases.sort.interfaces.ISortChain;
 
-public class SortChain<T> implements ISortChain<T> {
+public class SortChain<P> implements ISortChain {
 
-	private Comparator<Person> cmp;
-	
-	@Override
-	public int compare(Object o1, Object o2) {
-		this.cmp
-	}
+    ArrayList<Comparator> comparators = new ArrayList<Comparator>();
 
-	@Override
-	public void addComparator(Comparator arg0) {
-		this.cmp = arg0;
-	}
+    @Override
+    public int compare(Object o1, Object o2)
+    {
+        int result = 0;
+        for (int i = 0; i < comparators.size(); i++)
+        {
+            result = comparators.get(i).compare(o1, o2);
+            if (result != 0)
+                break;
+        }
+        return result;
+    }
+    
+    @Override
+    public void addComparator(Comparator comparator) {
+        comparators.add(comparator);
+    }
 
-	@Override
-	public void addComparator(int arg0, Comparator arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
+    @Override
+    public void addComparator(int i, Comparator comparator) {
+        comparators.add(i, comparator);
+    }
 }
