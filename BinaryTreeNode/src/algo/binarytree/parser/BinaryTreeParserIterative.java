2db42744f8c9d49f5609e37c12c9cd51ecccfc6e
Ismail NGUYEN <me@nguyenismail.com>
2015-10-07 15:11:36 +0200

    update iterative parsing

--- BinaryTreeNode/src/algo/binarytree/parser/BinaryTreeParserIterative.java
+++ BinaryTreeNode/src/algo/binarytree/parser/BinaryTreeParserIterative.java
@@ -6,30 +6,33 @@ import algo.binarytree.parser.interfaces.IParseWork;
 
 public class BinaryTreeParserIterative<T> implements IBinaryTreeParser<T> {
 
-    @Override
+	@Override
     public void parse(IBinaryTreeNode<T> iBinaryTreeNode, ParseMethod parseMethod, IParseWork<T>[] iParseWorks) {
 
         if(iBinaryTreeNode == null)
             return;
-
+                
         if(parseMethod == ParseMethod.INFIX){
-            parse(iBinaryTreeNode.getLeftChild(), parseMethod, iParseWorks);
-            //iBinaryTreeNode.getValue();
-            iParseWorks[0].execute(iBinaryTreeNode, 0, "");
-            parse(iBinaryTreeNode.getRightChild(), parseMethod, iParseWorks);
-
+        	IBinaryTreeNode<T> current = iBinaryTreeNode.getLeftChild();
+            while(current != null)
+            {
+            	iParseWorks[0].execute(current, 0, "");
+            	current = current.getRightChild();
+            }
         }else if(parseMethod == ParseMethod.PREFIX){
-            parse(iBinaryTreeNode.getLeftChild(), parseMethod, iParseWorks);
-            parse(iBinaryTreeNode.getRightChild(), parseMethod, iParseWorks);
-            iParseWorks[0].execute(iBinaryTreeNode, 0, "");
-            //iBinaryTreeNode.getValue();
-
+        	IBinaryTreeNode<T> current = iBinaryTreeNode.getLeftChild();
+            while(current != null)
+            {
+            	current = current.getRightChild();
+            	iParseWorks[0].execute(current, 0, ""); 	
+            }
         }else if (parseMethod == ParseMethod.SUFFIX){
-            parse(iBinaryTreeNode.getRightChild(), parseMethod, iParseWorks);
-            //iBinaryTreeNode.getValue();
-            iParseWorks[0].execute(iBinaryTreeNode, 0, "");
-            parse(iBinaryTreeNode.getLeftChild(), parseMethod, iParseWorks);
+        	IBinaryTreeNode<T> current = iBinaryTreeNode.getRightChild();
+            while(current != null)
+            {
+            	iParseWorks[0].execute(current, 0, "");
+            	current = current.getLeftChild();
+            }
         }
-
     }
 }
