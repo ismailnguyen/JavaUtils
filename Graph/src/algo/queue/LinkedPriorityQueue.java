d1b2622e65b6fe21bd2d196a5ba2f63cad6309db
Ismail NGUYEN <me@nguyenismail.com>
2015-10-12 23:45:12 +0200

    clean directory

--- Graph/src/algo/queue/LinkedPriorityQueue.java
+++ Graph/src/algo/queue/LinkedPriorityQueue.java
@@ -6,77 +6,64 @@ import algo.linkedlist.nodes.LinkSimple;
 import algo.linkedlist.nodes.interfaces.ILinkSimple;
 import algo.queue.interfaces.IPriorityQueue;
 
-public class LinkedPriorityQueue<V> implements IPriorityQueue<V> {
+public class LinkedPriorityQueue<T> implements IPriorityQueue<T> {
 
-	private ILinkSimple<V> head;
-	private ILinkSimple<V> current;
-	private Comparator<V> comparator;
-	
-	public LinkedPriorityQueue(Comparator<V> c)
-	{
-		this.comparator = c;
+	private ILinkSimple<T> node, queue;
+
+	private Comparator<T> cmp;
+
+	public LinkedPriorityQueue(Comparator<T> comparator) {
+		this.cmp = comparator;
 	}
-	
-	@Override
-	public void add(V arg0) {
-		ILinkSimple<V> tmp = new LinkSimple<V>();
-		tmp.setValue(arg0);
-		
-		if(this.head == null)	
-		{
-			this.head = tmp;
-			this.current = tmp;
+
+	public void add(T value) {
+		ILinkSimple<T> newNode = new LinkSimple<T>();
+		newNode.setValue(value);
+		if (node == null) {
+			this.node = newNode;
+			this.queue = newNode;
+		} else {
+
+			ILinkSimple<T> next = this.node;
+			ILinkSimple<T> prev = new LinkSimple<T>();
+			while (next != null && cmp.compare(value, next.getValue()) > 0) {
+				prev = next;
+				next = next.getNext();
+			}
+
+			if (next == null) {
+				queue.setNext(newNode);
+				queue = newNode;
+			} else {
+				if (prev.getValue() != null) {
+					prev.setNext(newNode);
+				} else {
+					this.node = newNode;
+				}
+				newNode.setNext(next);
+			}
 		}
+	}
+
+	public T peek() {
+		if (node != null)
+			return node.getValue();
 		else
-		{
-			ILinkSimple<V> old = this.head;
-			ILinkSimple<V> n = new LinkSimple<V>();
-			
-			while(n != null && this.comparator.compare(arg0, head.getValue()) < 0)
-			{
-				old = n;
-				n = n.getNext();
-			}
-			
-			if(old == null)
-			{
-				this.current.setNext(tmp);
-				this.current = tmp;
-			}
-			else
-			{
-				if(n.getValue() != null)
-				{
-					n.setNext(tmp);
-				}
-				else
-				{
-					this.head = tmp;
-				}
-				
-				tmp.setNext(old);
-			}
+			return null;
+	}
+
+	public T remove() {
+
+		if (node != null) {
+			T value = this.peek();
+
+			ILinkSimple<T> next = this.node.getNext();
+			this.node = next;
+
+			return value;
+		} else {
+			return null;
 		}
 	}
 
-	@Override
-	public V peek() {
-		if(this.head == null)
-			return null;
-		
-		return this.head.getValue();
-	}
-
-	@Override
-	public V remove() {
-		if(this.head == null)
-			return null;
-		
-		V tmp = this.peek();
-		
-		ILinkSimple<V> next = this.head.getNext();
-		this.head = next;
-		
-		return tmp;
-	}
 }
